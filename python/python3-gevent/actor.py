43553e57cf545d12776d759697c7bf5962270ba8
johnteee <johnteee@gmail.com>
2016-10-21 11:14:13 +0800

    Add Python3 gevent version

---
+++ python/python3-gevent/actor.py
@@ -0,0 +1,22 @@
+import gevent
+from gevent import Greenlet
+from gevent.queue import Queue
+
+class Actor(gevent.Greenlet):
+
+    def __init__(self):
+        self.inbox = Queue()
+        Greenlet.__init__(self)
+
+    def receive(self, message):
+        """
+        Define in your subclass.
+        """
+        raise NotImplemented()
+
+    def _run(self):
+        self.running = True
+
+        while self.running:
+            message = self.inbox.get()
+            self.receive(message)
---
+++ python/python3-gevent/main.py
@@ -0,0 +1,33 @@
+import gevent
+from gevent.queue import Queue
+from gevent import Greenlet
+from actor import Actor
+import time
+
+class Pinger(Actor):
+    def receive(self, message):
+        pong.inbox.put('ping')
+        gevent.sleep(0)
+
+class Ponger(Actor):
+    def receive(self, message):
+        global count, startTimestamp
+        count += 1
+        if count > 1000000:
+            print(time.time())
+            gevent.kill(ping)
+            gevent.kill(pong)
+        else :
+            ping.inbox.put('pong')
+        gevent.sleep(0)
+
+ping = Pinger()
+pong = Ponger()
+startTimestamp = int(time.time())
+count = 0
+
+ping.start()
+pong.start()
+
+ping.inbox.put('start')
+gevent.joinall([ping, pong])
